ac79423a09e0a759acdd5daaf22283b434ea65de
HUY <huy339093@gmail.com>
2023-03-29 07:40:08 +0700

    update_mongodb

--- testmongodb/connect.js
+++ testmongodb/connect.js
@@ -6,9 +6,10 @@ const app = express();
 
 
 //mongoose db connection
+
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://ducmong6969:Zaf0CdvLYyMkR8b0@maitienduc.gimicfu.mongodb.net/test_mongo?retryWrites=true&w=majority';
+const url = 'mongodb+srv://huynvph20687:c1sas2Dnu6fPDo6F@mongo-huynvph20687.f01mcft.mongodb.net/test_mongodb?retryWrites=true&w=majority';
 const sachModel = require('./sqlmodel');
 app.get('/sach', async (req, res) => {
     await mongoose.connect(url).then(console.log('kets noi db thanh cong'));
@@ -28,7 +29,7 @@ app.get("/update_sach", async (request, response) => {
     await mongoose.connect(url).then(console.log('Ket noi DB thanh cong.'));
 
     try {
-        var kq = await sachModel.updateOne({tensach: 'cuu am chan kinh'}, {tensach: 'võ thuật hong kong', gia: 25000});
+        var kq = await sachModel.updateOne({tensach: 'võ công tà '}, {tensach: 'võ công tà đạo', gia: 25000});
 
         console.log(kq);
 
@@ -60,7 +61,7 @@ app.get("/add_sach", async (request, response) => {
     await mongoose.connect(url).then(console.log('Ket noi DB thanh cong.'));
 
     let sach = new sachModel({
-        tensach: 'võ công',
+        tensach: 'võ công tà ',
         gia: 22322
     });
 
